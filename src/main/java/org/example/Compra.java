d86469480112284e279245e462b5f7658df3a86f
deimerin <deimer.castillo@correo.uis.edu.co>
2023-02-12 17:01:22 -0500

    registrar compra

--- src/main/java/org/example/Compra.java
+++ src/main/java/org/example/Compra.java
@@ -1,16 +1,41 @@
 package org.example;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Compra {
 
-    public void registrarCompra(Connection con, String nombre, Integer stock, Integer price){
+    public Integer calcularArqueo(Integer cPrice, Integer cStock, Integer nPrice, Integer nStock){
+        Integer nuevoPrecio = ((cPrice*cStock) + (nPrice*nStock) ) / (cStock + nStock);
+        return nuevoPrecio;
+    }
+
+    public void registrarCompra(Connection con, String nombre, Integer stock, Integer price) throws SQLException {
+
+        Integer cPrice = null;
+        Integer cStock = null;
+
+        Statement stm = con.createStatement();
+        String query = "SELECT price, stock FROM producto WHERE name = "+ String.format("'%s'", nombre);
+        ResultSet rs = stm.executeQuery(query);
+
+        if(rs.next()){
+            cPrice = rs.getInt("price");
+            cStock = rs.getInt("stock");
+        }
 
         PreparedStatement pstmt = null;
+        String sql = "UPDATE producto SET stock=?, price = ? WHERE name = ?";
 
-        String sql = "INSERT INTO producto(name, price, stock) VALUES (?, ?, ?)";
+        try {
+            pstmt = con.prepareStatement(sql);
+            pstmt.setInt(1, cStock+stock);
+            pstmt.setInt(2, calcularArqueo(cPrice,cStock,price,stock));
+            pstmt.setString(3, nombre);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
 
     }
 
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,34 @@
 package org.example;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
         DBC db = new DBC();
         Connection con = db.connect();
-
         Compra newCompra = new Compra();
 
-        newCompra.agregarProducto(con, "Headphones", 12, 300);
+        newCompra.registrarCompra(con,"Headphones",25,250);
+
+//        String nombre = "Headphones";
+//
+//        Statement pstmt = con.createStatement();
+//        String query = "SELECT price, stock FROM producto WHERE name = "+ String.format("'%s'", nombre);
+//
+//        ResultSet rs = pstmt.executeQuery(query);
+//        if (rs.next()) {
+//            System.out.println(rs.getInt("price"));
+//            System.out.println(rs.getInt("stock"));
+//        }
+
+//        Integer wer = newCompra.calcularArqueo(222,15,250,25);
+//        System.out.println(wer);
+
+
+
+
+
+
 
 
         //MainUI ui = new MainUI();
