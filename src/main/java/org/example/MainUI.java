1894b34ca067e69f7a070e6fc380a145d105038b
deimerin <deimer.castillo@correo.uis.edu.co>
2023-02-13 00:03:23 -0500

    tablas

--- src/main/java/org/example/MainUI.java
+++ src/main/java/org/example/MainUI.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.*;
@@ -23,12 +25,19 @@ public class MainUI extends JFrame {
     private JTextField cantidadProducto;
     private JButton agregarProductoButton;
     private JTextField precioProducto;
+    private JTable ventasTable;
+    private JTable invTable;
+    private JTable comTable;
 
     public void setUI() throws SQLException {
 
         DBC db = new DBC();
         Connection con = db.connect();
         Compra newCompra = new Compra();
+
+        setTableVentas(con);
+        setTableInven(con);
+        setTableCom(con);
 
         VariablesProducto varProducto = new VariablesProducto(nombreProducto, cantidadProducto, precioProducto);
 
@@ -87,5 +96,88 @@ public class MainUI extends JFrame {
 
     }
 
+    public void setTableVentas(Connection con) throws SQLException {
+        // Tabla Ventas
+        DefaultTableModel model = new DefaultTableModel();
 
+        ventasTable.setAutoCreateRowSorter(true);
+        ventasTable.setFillsViewportHeight(true);
+        ventasTable.setPreferredScrollableViewportSize(new Dimension(550, 200));
+        model.addColumn("ID");
+        model.addColumn("ID Cliente");
+        model.addColumn("ID Producto");
+        model.addColumn("Unidades");
+        model.addColumn("Total");
+        model.addColumn("Fecha");
+        ventasTable.setModel(model);
+
+        String query = "SELECT * FROM venta";
+        Statement stm = con.createStatement();
+        ResultSet rs = stm.executeQuery(query);
+
+        while(rs.next()){
+            model.addRow(new Object[]{
+                    rs.getInt("id"),
+                    rs.getInt("cliente_id"),
+                    rs.getInt("producto_id"),
+                    rs.getInt("unit"),
+                    rs.getInt("total"),
+                    rs.getDate("date")}
+            );
+        }
+    }
+
+    public void setTableInven(Connection con) throws SQLException {
+        // Tabla Inventory
+        DefaultTableModel model = new DefaultTableModel();
+
+        invTable.setAutoCreateRowSorter(true);
+        invTable.setFillsViewportHeight(true);
+        invTable.setPreferredScrollableViewportSize(new Dimension(550, 200));
+        model.addColumn("ID");
+        model.addColumn("Nombre");
+        model.addColumn("Precio");
+        model.addColumn("Stock");
+        invTable.setModel(model);
+
+        String query = "SELECT * FROM producto";
+        Statement stm = con.createStatement();
+        ResultSet rs = stm.executeQuery(query);
+
+        while(rs.next()){
+            model.addRow(new Object[]{
+                    rs.getInt("id"),
+                    rs.getString("name"),
+                    rs.getInt("price"),
+                    rs.getInt("stock")}
+            );
+        }
+    }
+
+    public void setTableCom(Connection con) throws SQLException {
+        // Tabla Inventory
+        DefaultTableModel model = new DefaultTableModel();
+
+        comTable.setAutoCreateRowSorter(true);
+        comTable.setFillsViewportHeight(true);
+        comTable.setPreferredScrollableViewportSize(new Dimension(550, 200));
+        model.addColumn("ID");
+        model.addColumn("ID Producto");
+        model.addColumn("Unidades");
+        model.addColumn("Precio");
+        comTable.setModel(model);
+
+        String query = "SELECT * FROM compra";
+        Statement stm = con.createStatement();
+        ResultSet rs = stm.executeQuery(query);
+
+        while(rs.next()){
+            model.addRow(new Object[]{
+                    rs.getInt("id"),
+                    rs.getInt("producto_id"),
+                    rs.getInt("unidades"),
+                    rs.getInt("precio")}
+            );
+        }
+    }
 }
