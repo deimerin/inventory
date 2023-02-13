95418723962dd17fe9393ccb7b5b1cb6b578ea11
deimerin <deimer.castillo@correo.uis.edu.co>
2023-02-12 19:49:58 -0500

    arreglo funcion compra

--- src/main/java/org/example/Compra.java
+++ src/main/java/org/example/Compra.java
@@ -13,14 +13,16 @@ public class Compra {
 
         Integer cPrice = null;
         Integer cStock = null;
+        Integer pID = null;
 
         Statement stm = con.createStatement();
-        String query = "SELECT price, stock FROM producto WHERE name = "+ String.format("'%s'", nombre);
+        String query = "SELECT price, stock, id FROM producto WHERE name = "+ String.format("'%s'", nombre);
         ResultSet rs = stm.executeQuery(query);
 
         if(rs.next()){
             cPrice = rs.getInt("price");
             cStock = rs.getInt("stock");
+            pID = rs.getInt("id");
         }
 
         PreparedStatement pstmt = null;
@@ -36,12 +38,25 @@ public class Compra {
             throw new RuntimeException(e);
         }
 
+        String sql2 = "INSERT INTO compra(producto_id, unidades, precio) VALUES (?,?,?)";
+
+        try {
+            pstmt = con.prepareStatement(sql2);
+            pstmt.setInt(1, pID);
+            pstmt.setInt(2, stock);
+            pstmt.setInt(3, price);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
-    public void agregarProducto(Connection con, String nombre, Integer stock, Integer price){
+    public void agregarProducto(Connection con, String nombre, Integer stock, Integer price) throws SQLException {
 
         PreparedStatement pstmt = null;
+        Statement stm = con.createStatement();
+        Integer pID = null;
 
         String sql = "INSERT INTO producto(name, price, stock) VALUES (?, ?, ?)";
         try {
@@ -53,5 +68,28 @@ public class Compra {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+
+        String query = "SELECT id FROM producto WHERE name = "+ String.format("'%s'", nombre);
+        ResultSet rs = stm.executeQuery(query);
+
+        if(rs.next()){
+            pID = rs.getInt("id");
+        }
+
+        String sql2 = "INSERT INTO compra(producto_id, unidades, precio) VALUES (?,?,?)";
+
+        try {
+            pstmt = con.prepareStatement(sql2);
+            pstmt.setInt(1, pID);
+            pstmt.setInt(2, stock);
+            pstmt.setInt(3, price);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,34 +4,15 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        //DBC db = new DBC();
-        //Connection con = db.connect();
-        //Compra newCompra = new Compra();
+        DBC db = new DBC();
+        Connection con = db.connect();
+        Compra newCompra = new Compra();
+
+        newCompra.agregarProducto(con, "Notebook", 25, 15);
 
         //Venta newVenta = new Venta();
-
         //newVenta.registrarVenta(con,"Joyce", "Headphones", 3);
-
-        //newCompra.registrarCompra(con,"Headphones",25,250);
-
-//        String nombre = "Headphones";
-//
-//        Statement pstmt = con.createStatement();
-//        String query = "SELECT price, stock FROM producto WHERE name = "+ String.format("'%s'", nombre);
-//
-//        ResultSet rs = pstmt.executeQuery(query);
-//        if (rs.next()) {
-//            System.out.println(rs.getInt("price"));
-//            System.out.println(rs.getInt("stock"));
-//        }
-
-//        Integer wer = newCompra.calcularArqueo(222,15,250,25);
-//        System.out.println(wer);
-
-
-
-
-
+        //newCompra.registrarCompra(con,"Headphones",15,255);
 
 
 
